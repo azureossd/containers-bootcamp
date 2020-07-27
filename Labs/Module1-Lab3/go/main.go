56293a633b6d0a9dbec547a884f47f3f57389701
Edison Garcia Chinas <edisga@microsoft.com>
2020-07-26 19:37:40 -0500

    Adding Go Language

--- Labs/Module1-Lab3/go/main.go
+++ Labs/Module1-Lab3/go/main.go
@@ -4,13 +4,32 @@ import (
     "fmt"
     "os"
     "net/http"
+    "encoding/json"
 )
 
+type App struct {
+	Message        string `json:"Message"`
+	HostName       string `json:"HostName"`
+    RequestHeaders map[string][]string `json:"RequestHeaders"`
+}
+
 func main() {
-    http.HandleFunc("/", HelloServer)
+    http.HandleFunc("/", MainServer)
     http.ListenAndServe(":8080", nil)
 }
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Reading env variable: %s!", os.Getenv("apiURL"))
+func MainServer(w http.ResponseWriter, r *http.Request) {
+
+    hostname, _ := os.Hostname()
+    headers := r.Header
+
+    data := App{
+        Message: "My go container is running...",
+        HostName: hostname,
+        RequestHeaders: headers,
+    }
+    
+    buf, _ := json.Marshal(data)
+    fmt.Fprintf(w, string(buf))
+
 }
